a2ca7d4a15dd67f9d1a4fd98fb9eceb220f5b32d
Salsa Gal <fielddashos@gmail.com>
2022-04-09 20:48:57 +1000

    Remove bad old code for FAT

--- src/bootload.asm
+++ src/bootload.asm
@@ -1,29 +1,2 @@
-mov ax, 0x07c0
-mov ds, ax
-mov es, ax
-
-mov si, message_start
-call print
-
-jmp $
-
-; si - string pointer
-print:
-    push ax
-    mov ah, 0xe
-
-.loop:
-    lodsb
-    cmp al, 0
-    je .end
-    int 0x10
-    jmp .loop
-
-.end:
-    pop ax
-    ret
-
-message_start: db "Bail: Started", 0
-
 times 510-($-$$) db 0
 dw 0xaa55
